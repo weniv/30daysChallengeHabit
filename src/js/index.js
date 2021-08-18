2371471f193e3630d9e88d4f220cd3a0b199b77d
WonBeomChoi <cwb2310@gmail.com>
2021-08-18 17:04:45 +0900

    init dom & localStorage-Data

--- src/js/index.js
+++ src/js/index.js
@@ -20,3 +20,37 @@ window.onclick = function(e) {
           }
     }
 };
+
+function init() {
+    if (!localStorage.getItem("habitChallengeData")){
+        console.log("생성됨");
+        defaultData = {};
+        [...Array(30)].forEach((k,i)=>{
+            defaultData[i+1] = 0
+        })
+        
+        localStorage.setItem("habitChallengeData", JSON.stringify({
+            title:"제목을 설정해주세요",
+            data:defaultData
+        }))
+    }
+    
+    const successList = JSON.parse(localStorage.getItem("habitChallengeData"))
+    console.log(successList.data);
+    
+    const app = document.getElementsByClassName("table-item-wrap")[0]
+    app.innerHTML=""
+    console.log(app);
+    for (const key in successList.data) {
+        if (Object.hasOwnProperty.call(successList.data, key)) {
+            const stamp = successList.data[key];
+            const el = document.createElement("div");
+            el.className = "table-item";
+            el.innerHTML = key
+            el.setAttribute('data-value', stamp)
+            app.appendChild(el)
+        }
+    }
+}
+
+init()
