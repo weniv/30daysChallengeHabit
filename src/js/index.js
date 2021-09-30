9d2a1532825ffae9280ed179f18e6d5e62a7c270
zeenee <dbwls982@gmail.com>
2021-09-30 14:38:21 +0900

    스타일 변경

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,7 @@ const saveBtn = document.querySelector('.save-btn');
 
 window.addEventListener("keydown",function(e) {
     if(e.keyCode===13){
-        e.preventDefault()
+        e.preventDefault();
     }
 })
 
@@ -59,7 +59,8 @@ function createDom(dom="div",id="",className="", child="") {
     el.className = className;
     el.id=id;
     el.append(child);
-    return el
+
+    return el;
 }
 function progressCheck() {
     const items = document.getElementsByClassName("table-item");
@@ -70,47 +71,46 @@ function progressCheck() {
     progressBar.style.width = `${count/items.length*100}%`
 }
 function saveAppData() {
-    localStorage.setItem("habitChallengeData", JSON.stringify(appData))
-    progressCheck()
+    localStorage.setItem("habitChallengeData", JSON.stringify(appData));
+    progressCheck();
 }
 
 //일정에 도장찍기
 function setSticker(idx) {
-    const selectedDom = document.getElementById(`item${selectedItem+1}`)
-    selectedDom.dataset.value = idx
+    const selectedDom = document.getElementById(`item${selectedItem+1}`);
+    selectedDom.dataset.value = idx;
     selectedDom.innerHTML=""
-    const sticker = stickers[idx].cloneNode()
-    sticker.addEventListener('click', addModalEvt)
-    selectedDom.append(sticker)
-    appData['data'][selectedItem+1] = idx
-    saveAppData()
+    const sticker = stickers[idx].cloneNode();
+    sticker.addEventListener('click', addModalEvt);
+    selectedDom.append(sticker);
+    appData['data'][selectedItem+1] = idx;
+    saveAppData();
 }
 
 
 //챌린지 설정
 function setChallenge() {
-    const challengeSetting = new FormData(document.getElementById("setChallengeForm"))
-    const datas = Array.from(challengeSetting)
+    const challengeSetting = new FormData(document.getElementById("setChallengeForm"));
+    const datas = Array.from(challengeSetting);
     const prev_challengeName = appData.challengeName
     for (const i in datas) {
         if (Object.hasOwnProperty.call(datas, i)) {
             const data = datas[i];
-            appData[data[0]] = data[1]
+            appData[data[0]] = data[1];
         }
     }
     settingModal.classList.remove("active");
     if(prev_challengeName != appData.challengeName){
         const defaultData = {};
         [...Array(30)].forEach((k,i)=>{
-            defaultData[i+1] = 4
+            defaultData[i+1] = 4;
         })
-        appData.data = defaultData
+        appData.data = defaultData;
         document.getElementsByClassName("challenge-title")[0].innerHTML = appData.challengeName;
         document.getElementById("start-date").innerHTML = appData.startDate;
     }
-    setTable()
-    saveAppData()
-
+    setTable();
+    saveAppData();
 }
 
 function addModalEvt(item, idx) {
@@ -139,21 +139,21 @@ function addModalEvt(item, idx) {
 }
 
 function setTable() {
-    app.innerHTML=""
+    app.innerHTML="";
     for (const key in appData.data) {
         if (Object.hasOwnProperty.call(appData.data, key)) {
             const stamp = appData.data[key];
-            const el = createDom(dom="div", id=`item${key}`,className="table-item", child= stamp==4 ? key : stickers[stamp].cloneNode())
-            el.setAttribute('data-value', stamp)
-            app.append(el)
+            const el = createDom(dom="div", id=`item${key}`,className="table-item", child= stamp==4 ? key : stickers[stamp].cloneNode());
+            el.setAttribute('data-value', stamp);
+            app.append(el);
         }
         if (key==appData.challengeTerm){
-            break
+            break;
         }
     }
     const tableItem = document.querySelectorAll(".table-item");
     tableItem.forEach((item, idx) => {
-        addModalEvt(item,idx)
+        addModalEvt(item,idx);
     });
 
 
@@ -166,7 +166,7 @@ function init() {
     if (!localStorage.getItem("habitChallengeData")){
         const defaultData = {};
         [...Array(30)].forEach((k,i)=>{
-            defaultData[i+1] = 4
+            defaultData[i+1] = 4;
         })
         const date = new Date
         localStorage.setItem("habitChallengeData", JSON.stringify({
@@ -174,38 +174,36 @@ function init() {
             data:defaultData,
             challengeTerm:25,
             startDate:date.toDateString()
-        }))
+        }));
     }
-    appData = JSON.parse(localStorage.getItem("habitChallengeData"))
-    document.getElementById("challenge-name").value = appData.challengeName
-    document.getElementById(`day_${appData.challengeTerm}`).checked = true
-    document.getElementById('')
-    document.getElementsByClassName("challenge-title")[0].innerHTML = appData.challengeName
-    document.getElementById("start-date").innerHTML = appData.startDate
+    appData = JSON.parse(localStorage.getItem("habitChallengeData"));
+    document.getElementById("challenge-name").value = appData.challengeName;
+    document.getElementById(`day_${appData.challengeTerm}`).checked = true;
+    document.getElementById('');
+    document.getElementsByClassName("challenge-title")[0].innerHTML = appData.challengeName;
+    document.getElementById("start-date").innerHTML = appData.startDate;
 
     // const app = document.getElementsByClassName("table-item-wrap")[0]
 
-    setTable()
-    progressCheck()
+    setTable();
+    progressCheck();
 }
 
-
-init()
-
+init();
 
 
 // 스티커 추가 이벤트
+const tableItem = document.querySelectorAll(".table-item");
 const challengeTable = document.querySelector(".challenge-table");
 const selectSticker = document.querySelector(".select-sticker");
 
 
 stickers.forEach((item,idx)=>{
-    const idx1 = idx
+    const idx1 = idx;
     item.addEventListener("click", ()=>{
-        setSticker(idx1)
+        setSticker(idx1);
     })
 })
-
 
 
 // 스티커 선택창 스타일 변경ww
@@ -213,20 +211,20 @@ stickers.forEach((item,idx)=>{
 function stickerStyle(i, e){
     // margin 값을 포함한 tableItem의 width값
     const tableItemWidth = e.offsetWidth + parseInt(window.getComputedStyle(e).getPropertyValue("margin-bottom"), 10);
-    const index = i%5;
-    selectSticker.style.top = i<16 ? `${tableItemWidth+e.offsetTop}px` : `${e.offsetTop-selectSticker.offsetHeight}px`;
-    selectSticker.style.left = index <= 1?`${e.offsetLeft}px`: `${e.offsetLeft-e.offsetWidth*index-e.style.marginTop}px`;
-    selectedItem = i
+    const index = i % 5;
+    selectSticker.style.top = i < 16 ? `${tableItemWidth+e.offsetTop}px` : `${e.offsetTop-selectSticker.offsetHeight}px`;
+    selectSticker.style.left = index <= 1 ? `${e.offsetLeft}px` : `${tableItem[1].offsetLeft}px`;
+    selectedItem = i;
 }
 
 //스크린샷 기능
 async function screenShot() {
-    const cv = await html2canvas(document.body)
-    const imgData = cv.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream")
-    const vDom = document.createElement('a')
-    vDom.href = imgData
-    vDom.download = "myChallenge.jpg"
-    vDom.click()
+    const cv = await html2canvas(document.body);
+    const imgData = cv.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+    const vDom = document.createElement('a');
+    vDom.href = imgData;
+    vDom.download = "myChallenge.jpg";
+    vDom.click();
 }
 
 function shareFacebook() {
@@ -242,4 +240,4 @@ function shareTwitter() {
     let twitter = "https://twitter.com/intent/tweet?text="
     window.open(twitter + sendText + "&url=" + url);
 }
-saveBtn.addEventListener('click',screenShot)
+saveBtn.addEventListener('click',screenShot);
